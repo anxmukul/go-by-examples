87b2f794e0b86461b5e3fd23e4950ffe3eb69c48
anxmukul <kmukul1197@gmail.com>
2023-05-15 15:27:30 +0530

    Methods and Interface

---
+++ interfaces.go
@@ -0,0 +1,60 @@
+//  Lets say we have a companywho wants to calculate total expances based on their employee salary
+// and the company have two type of emp. 1. Permanent, 2. Custom
+
+package main
+import "fmt"
+
+type salaryCalcultor interface{
+	calculateSalary() int
+}
+
+type Permanent struct {
+	id int
+	basicSalary int
+	pf int
+}
+
+type Contract struct {
+	id  int
+	basicSalary int
+}
+
+func (p Permanent) calculateSalary() int {
+	return p.basicSalary + p.pf
+}
+
+func (c Contract) calculateSalary() int {
+	return c.basicSalary
+}
+
+func totalExpances(s []salaryCalcultor) int {
+	expances := 0
+	for _, emp := range s {
+		expances += emp.calculateSalary()
+	}
+
+	// fmt.Println("Total Company Expances is: ", expances)
+	return expances
+}
+func main(){
+	pemp1 := Permanent{1, 250000, 23000}
+	pemp2 := Permanent{2, 450000, 13000}
+	cemp1 := Contract{3, 65000}
+	cemp2 := Contract{4, 89000}
+	pemp3 := Permanent{5, 650000, 6500}
+	// Now if we want to calculate the total expances we have to read all the emp salary individually and add them
+	
+	// totalExpances := pemp1.calculateSalary() +pemp2.calculateSalary() + cemp1.calculateSalary() + cemp2.calculateSalary();
+	// fmt.Println("Total expances: ", totalExpances);
+	
+	// Although Go automatically handles conversion between values and pointers for method calls. It's still lengthy
+
+	// Here comes the interface :- Now we will crete an interface (an interface is a set of method signatures.)
+	// create a single calculateexpances method and pass slice of employee object to that method and interface will
+	// Automatically calculate the total expances
+
+
+	employees := [] salaryCalcultor {pemp1, pemp2, cemp1, cemp2, pemp3}
+	fmt.Println("Total Company Expances is: ", totalExpances(employees))
+
+}
--- todo.go
+++ todo.go
@@ -1,31 +1,36 @@
 package main 
 import "fmt"
 
-type task struct {
+type Todo struct {
 	id int
 	todo string
 	priority string
 }
 var i = 1;
-func newTask(todo string, priority string) *task {
-	var t task
-	t.id = i
-	i++
-	t.todo = todo
-	t.priority = priority
-
-	return &t;
+func (tsk *Todo) createTodo(todo string, priority string) {
+	tsk.id = i
+	tsk.todo = todo
+	tsk.priority = priority
+	i++;
 }
 
-// func showTask(n task){
-// 	fmt.Println("Id:", n.task)
-// 	fmt.Println("Todo: ", n.todo)
-// 	fmt.Println("Priority: ", n.priority);
-// }
+ func (tsk Todo) showTask(){
+	// fmt.Println("Id:", tsk.id)
+	// fmt.Println("Todo: ", tsk.todo)
+	// fmt.Println("Priority: ", tsk.priority);
+	fmt.Printf("Task\n Id: %v\n Todo: %v\n Priority: %v\n", tsk.id, tsk.todo, tsk.priority)
+
+}
 func main(){
-	var task1 task;
-	task1 = newTask("Call Abhisek", "Low")
-	fmt.Println(task1)
-	// newTask("Comple Go by examples", "High")
-	// newTask("Learn more about structure in go", "Medium")
+	task1 := &Todo{}
+	task1.createTodo( "Call Abhishek", "Low");
+	task2 := &Todo{}
+	task2.createTodo( "Complete Go by Example", "Medium");
+	task3 := &Todo{}
+	task3.createTodo( "Start Open Source", "High");
+	task1.showTask()
+	task2.showTask()
+	task3.showTask()
+	
+	
 }
