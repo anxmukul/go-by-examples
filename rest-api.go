3e59f976c1f6b3fc4ad148319fdbc3aab01d67f3
anxmukul <kmukul1197@gmail.com>
2023-06-14 16:35:46 +0530

    Reading returned rows from select query and displaying it properly

--- rest-api.go
+++ rest-api.go
@@ -1,24 +1,26 @@
 package main
+
 import (
-	"fmt"
-	"net/http"
 	"database/sql"
+	"fmt"
 	_ "github.com/lib/pq"
+	"net/http"
 )
-var db *sql.DB;
+
+var db *sql.DB
 
 const (
-    host     = "localhost"
-    port     = 5432
-    user     = "postgres"
-    password = ""
-    dbname   = "employee"
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "blah"
+	dbname   = "employee"
 )
 
-func main(){
-	fmt.Println("This is a rest-api......");
-	http.HandleFunc("/", homePage);
-	http.HandleFunc("/insert", insertRow);
+func main() {
+	fmt.Println("This is a rest-api......")
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/insert", insertRow)
 	http.HandleFunc("/getdata", readFromTable)
 	http.HandleFunc("/updaterow", updateRow)
 	http.HandleFunc("/deleterow", deleteRow)
@@ -27,71 +29,82 @@ func main(){
 
 	var err error
 
-    db, err = sql.Open("postgres", psqlconn)
+	db, err = sql.Open("postgres", psqlconn)
 	fmt.Println(db)
 
-    if(err != nil){
-		panic(err);
-	}
-     
-    defer db.Close()
- 
-        // check db
-    err = db.Ping()
-    if(err != nil){
+	if err != nil {
 		panic(err)
 	}
- 
-    fmt.Println("Connected to db.....")
+
+	defer db.Close()
+
+	// check db
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to db.....")
 	if err := http.ListenAndServe(":3001", nil); err != nil {
 		panic(err)
 	}
 	// fmt.Println("Line 42");
 }
 
-func homePage(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "Welcome to homepage!!");
-}
-func readFromTable(w http.ResponseWriter, r *http.Request){
-	selectQuery := `select * from employeedetail`;
-	data, err := db.Query(selectQuery);
-	if(err != nil){
-		panic(err)
-	}
-	fmt.Fprint(w, "Employee table data fetched");
-	fmt.Println(data);
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome to homepage!!")
 }
 
-func insertRow(w http.ResponseWriter, r *http.Request){
+type emp struct {
+	Emp_id    int
+	Emp_name  string
+	Emp_email string
+	Emp_dep   string
+}
+
+// emp_name := "";
+// emp_email := "";
+// emp_dep := "";
+func readFromTable(w http.ResponseWriter, r *http.Request) {
+	var returnedRow emp
+	selectQuery := `select * from employeedetail where emp_id = 3`
+	err := db.QueryRow(selectQuery).Scan(&returnedRow.Emp_id, &returnedRow.Emp_name, &returnedRow.Emp_email, &returnedRow.Emp_dep)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprint(w, "Employee table data fetched")
+	fmt.Println(returnedRow)
+}
+
+func insertRow(w http.ResponseWriter, r *http.Request) {
 	// fmt.Println("in handler")
 
 	insertQuery := `insert into employeedetail (emp_name, emp_email, emp_dep) values ('Ravi', 'ravi@email.com', 'eng');`
-	
+
 	// fmt.Println("db obj:", db);
 	// fmt.Println("in handler 2")
-	
-	_, err := db.Query(insertQuery);
-	if(err != nil){
+	_, err := db.Query(insertQuery)
+	if err != nil {
 		// fmt.Println(err);
-		panic(err);
+		panic(err)
 	}
-	fmt.Fprint(w, "Row Inserted");
+	fmt.Fprint(w, "Row Inserted with emp_no")
 }
 
-func updateRow(w http.ResponseWriter, r *http.Request){
+func updateRow(w http.ResponseWriter, r *http.Request) {
 	updateQuery := `update employeedetail set emp_email = 'priyans@rediffmail.com' where emp_id = 3;`
-	_, err := db.Query(updateQuery);
-	if(err!= nil){
+	_, err := db.Query(updateQuery)
+	if err != nil {
 		panic(err)
 	}
-	fmt.Fprint(w, "Row updated");
+	fmt.Fprint(w, "Row updated")
 }
 
-func deleteRow(w http.ResponseWriter, r *http.Request){
+func deleteRow(w http.ResponseWriter, r *http.Request) {
 	deleteQuery := `delete from employeedetail where emp_name = 'Ravi';`
-	_, err := db.Query(deleteQuery);
-	if(err != nil){
+	_, err := db.Query(deleteQuery)
+	if err != nil {
 		panic(err)
 	}
-	fmt.Fprint(w, "Row Deleted");
+	fmt.Fprint(w, "Row Deleted")
 }
